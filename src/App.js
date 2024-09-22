357e4188e7cd2ee7639d7f6b2d02a7b99ce37057
jiyeon <namjy04@naver.com>
2024-09-22 15:59:43 +0900

    소개 페이지 디자인 1차 완성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useState, useEffect } from "react";
 import GlobalStyle from "./pages/GlobalStyles";
 import Talk from "./pages/Talk";
+import Introduce_ARETEUM from "./pages/Introduce_ARETEUM";
+import Introduce_Management from "./pages/Introduce_Management";
+import Introduce_Lion from "./pages/Introduce_Lion";
 
 function App() {
   return (
@@ -9,6 +12,12 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/talk" element={<Talk />} />
+        <Route path="/introduce_areteum" element={<Introduce_ARETEUM />} />
+        <Route
+          path="/introduce_management"
+          element={<Introduce_Management />}
+        />
+        <Route path="/introduce_lion" element={<Introduce_Lion />} />
       </Routes>
     </BrowserRouter>
   );
