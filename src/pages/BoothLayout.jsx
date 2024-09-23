5695f0e72f8851980b34cf858d94f149b7410c10
ha <hcastle04@gamil.com>
2024-09-24 00:44:10 +0900

    _

--- src/pages/BoothLayout.jsx
+++ src/pages/BoothLayout.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import * as BL from "../styles/StyledBoothLayout";
 
@@ -10,10 +10,21 @@ const BoothLayout = () => {
   };
 
   useEffect(() => {
-    // 컴포넌트가 마운트되면 상단으로 스크롤
     window.scrollTo(0, 0);
   }, []);
 
+  // 이미지 상태 관리
+  const [isClicked1001, setIsClicked1001] = useState(true);
+  const [isClicked1002, setIsClicked1002] = useState(false);
+
+  const handleClick1001 = () => {
+    setIsClicked1001(true);
+    setIsClicked1002(false);
+  };
+  const handleClick1002 = () => {
+    setIsClicked1002(true);
+    setIsClicked1001(false);
+  };
   return (
     <BL.Container>
       <BL.Header>
@@ -33,20 +44,46 @@ const BoothLayout = () => {
           alt="background"
         />
       </BL.Background>
-      <BL.Wrap>
-        <BL.Date>
-          <img
-            id="1001"
-            src={`${process.env.PUBLIC_URL}/images/BoothLayout/Clicked1001.svg`}
-            alt="1001"
-          />
-          <img
-            id="1002"
-            src={`${process.env.PUBLIC_URL}/images/BoothLayout/unClicked1002.svg`}
-            alt="1002"
-          />
-        </BL.Date>
-      </BL.Wrap>
+      <BL.Date>
+        <img
+          id="1001"
+          src={`${process.env.PUBLIC_URL}/images/BoothLayout/${
+            isClicked1001 ? "Clicked1001.svg" : "unClicked1001.svg"
+          }`}
+          alt="1001"
+          onClick={handleClick1001}
+        />
+        <img
+          id="1002"
+          src={`${process.env.PUBLIC_URL}/images/BoothLayout/${
+            isClicked1002 ? "Clicked1002.svg" : "unClicked1002.svg"
+          }`}
+          alt="1002"
+          onClick={handleClick1002}
+        />
+      </BL.Date>
+      <BL.BoothImg>
+        <img
+          src={`${process.env.PUBLIC_URL}/images/BoothLayout/BoothImg.svg`}
+          alt="boothimg"
+        />
+      </BL.BoothImg>
+      <BL.Buttons>
+        <button id="day1">1</button>
+        <button id="day1">2</button>
+        <button id="day1">3</button>
+        <button id="day1">4</button>
+        <button id="day1">5</button>
+      </BL.Buttons>
+      <BL.BoothInfo>
+        <BL.Box>
+          <BL.DAY1></BL.DAY1>
+          <BL.DAY1></BL.DAY1>
+        </BL.Box>
+      </BL.BoothInfo>
+      <BL.Detail>
+        <div id="detail">부스 상세정보</div>
+      </BL.Detail>
       <BL.Footer>
         <object
           data={`${process.env.PUBLIC_URL}/images/Footer.svg`}
--- src/styles/StyledBoothLayout.jsx
+++ src/styles/StyledBoothLayout.jsx
@@ -2,28 +2,28 @@ import { styled } from "styled-components";
 
 export const Container = styled.div`
   position: relative;
+  diplay: flex;
+  justify-content: center;
+  align-items: center;
   margin: 0 auto;
   width: 393px;
-  height: 852px;
+  height: 1113px;
   margin-top: 0px;
   background: linear-gradient(180deg, #0c2456 0%, #0a759f 100%);
   min-height: 100vh;
   padding: 0; /* 불필요한 패딩 제거 */
   box-sizing: border-box; /* 패딩이 width에 포함되도록 설정 */
+  overflow: hidden;
+  z-index: 0;
 `;
 
 export const Background = styled.div`
   position: absolute; /* 상대 위치 설정으로 자연스럽게 배치 */
-  //   margin-top: 70px; /* Header의 높이만큼 띄워줍니다. */
   width: 393px;
   height: 737px;
   flex-shrink: 0;
-  z-index: 0; /* 다른 요소들 뒤에 배치 */
-
+  z-index: 1;
   img {
-    padding-top: 95px;
-    // width: 100%;
-    // height: 100%;
     object-fit: cover; /* 배경 이미지가 화면에 맞게 늘어나도록 설정 */
   }
 `;
@@ -59,18 +59,105 @@ export const Title = styled.div`
   transform: translateX(-50%);
 `;
 
-export const Wrap = styled.div`
-  position: relation;
-  diplay: flex;
+export const Date = styled.div`
+  position: relative;
+  display: flex;
   justify-content: center;
   align-items: center;
-`;
-export const Date = styled.div`
-  diplay: flex;
-  justify-content: center;
+  z-index: 2;
+  margin-top: 16px;
+  gap: 90px;
   img {
   }
 `;
+export const BoothImg = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
+  margin-top: 26px;
+`;
+export const Buttons = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
+
+  gap: 8.5px;
+  margin-top: 18px;
+
+  button {
+    width: 60px;
+    height: 40px;
+    flex-shrink: 0;
+    border-radius: 30px;
+    background: #fffdf0;
+    border: none;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+
+    color: #0c2557;
+    text-align: center;
+    font-family: "Pretendard Variable";
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+  }
+`;
+export const BoothInfo = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
+`;
+
+export const Box = styled.div`
+  width: 334px;
+  height: 298px;
+  flex-shrink: 0;
+  border-radius: 20px;
+  background: rgba(255, 255, 255, 0.7);
+
+  margin-top: 29px;
+  margin-bottom: 30px;
+`;
+export const DAY1 = styled.div``;
+export const DAY2 = styled.div``;
+export const Detail = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 115px;
+    height: 39px;
+    padding: 10px 7px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    flex-shrink: 0;
+
+    border-radius: 30px;
+    background: #fbefb5;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+
+    color: #0c2557;
+    font-family: "Pretendard Variable";
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+  }
+`;
+
 export const Footer = styled.footer`
   width: 393px;
   height: 290px;
