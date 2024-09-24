a0f387cebb8368c47737de57b7e0831eecc79d55
minyeong3201 <164001002+minyeong3201@users.noreply.github.com>
2024-09-25 01:41:16 +0900

    부스검색 final

--- src/pages/Keyword.jsx
+++ src/pages/Keyword.jsx
@@ -12,13 +12,8 @@ const Keyword = () => {
   const [category, setCategory] = useState(null);
   const [totalResults, setTotalResults] = useState(0);
 
-  useEffect(() => {
-    // 컴포넌트가 마운트되면 상단으로 스크롤
-    window.scrollTo(0, 0);
-  }, []);
-
   const goback = () => {
-    window.history.back();
+    navigate(`/`);
   };
 
   const gosearch = () => {
@@ -127,22 +122,12 @@ const Keyword = () => {
       <object
         data={`${process.env.PUBLIC_URL}/images/E.svg`}
         alt="Up2Star"
-        style={{
-          position: "absolute",
-          top: "110px",
-          left: "30px",
-          width: "40px",
-        }}
+        style={{ position: "absolute", top: "110px", left: "30px", width: "40px" }}
       />
       <object
         data={`${process.env.PUBLIC_URL}/images/E.svg`}
         alt="Up1Star"
-        style={{
-          position: "absolute",
-          top: "78px",
-          left: "0px",
-          width: "70px",
-        }}
+        style={{ position: "absolute", top: "78px", left: "0px", width: "70px" }}
       />
       <object
         data={`${process.env.PUBLIC_URL}/images/E.svg`}
@@ -157,22 +142,12 @@ const Keyword = () => {
       <object
         data={`${process.env.PUBLIC_URL}/images/E.svg`}
         alt="Under1Star"
-        style={{
-          position: "absolute",
-          top: "260px",
-          right: "50px",
-          width: "50px",
-        }}
+        style={{ position: "absolute", top: "260px", right: "50px", width: "50px" }}
       />
       <object
         data={`${process.env.PUBLIC_URL}/images/A.svg`}
         alt="YBouble"
-        style={{
-          position: "absolute",
-          top: "90px",
-          right: "0px",
-          width: "70px",
-        }}
+        style={{ position: "absolute", top: "90px", right: "0px", width: "70px" }}
       />
       <K.Box>
         <K.Box2>
