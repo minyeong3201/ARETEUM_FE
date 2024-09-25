47815ca9f06565b6ba35b29982c261ab48ad9baf
ha <hcastle04@gamil.com>
2024-09-25 16:56:22 +0900

    부스배치도 효과 추가

--- src/styles/StyledBoothLayout.jsx
+++ src/styles/StyledBoothLayout.jsx
@@ -80,6 +80,10 @@ export const Date = styled.div`
   margin-top: 16px;
   gap: 90px;
   img {
+    transition: filter 0.3s ease;
+    &.active {
+      filter: drop-shadow(0px 0px 20px #fff);
+    }
     cursor: pointer;
   }
 `;
